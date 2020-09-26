f47e6627ad4cfe220b02463ced66cc0d5df6e8c3
Gautham245 <gauthamprabhu245@gmail.com>
2020-09-26 17:09:08 +0530

    Added About us page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
@@ -48,6 +49,7 @@ class Main extends Component {
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />}/>
           <Redirect to="/home" />
         </Switch>
         <Footer />
