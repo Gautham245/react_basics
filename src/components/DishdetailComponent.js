67c7fb1a63935bbaedab07bad1a6cc9503aa287f
Gautham245 <gauthamprabhu245@gmail.com>
2020-09-26 16:07:06 +0530

    Added nav bar , react router, home component and contact component

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay,CardText, CardBody,CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 function RenderDish({dish})
 {
   if (dish != null)
@@ -17,10 +19,10 @@ function RenderDish({dish})
         <div></div>
     );
 }
-function RenderComments({dish})
+function RenderComments({comments})
 {
-  if(dish!=null){
-    var commentList = dish.comments.map(comment => {
+  if(comments!=null){
+    var commentList = comments.map(comment => {
       return (
         <ul key={comment.id} className="list-unstyled">
           <li>{comment.comment}</li>
@@ -37,6 +39,7 @@ function RenderComments({dish})
         </CardTitle>
         <CardBody>
             <CardText>{commentList}</CardText>
+            <br/><br/>
         </CardBody>
       </Card>
     );
@@ -48,13 +51,26 @@ function RenderComments({dish})
 }
 const Dishdetail = (props) => {
   return (
-    <div className="row">
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+        </div>
+      </div>
+      <div className="row">
         <div className="col-12 col-md-5 m-1">
-          <RenderDish dish={props.dish} />
+            <RenderDish dish={props.dish} />
         </div>
         <div className="col-12 col-md-5 m-1">
-          <RenderComments dish={props.dish} />
+            <RenderComments comments={props.comments} />
         </div>
+      </div>
     </div>
   );
 }
