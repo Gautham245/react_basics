f5d53e05fc8873924e105e9017e13da99cce2240
Gautham245 <gauthamprabhu245@gmail.com>
2020-10-26 14:36:05 +0530

    error message added if server cannot fetch data(error message in home component not working)

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -12,7 +12,7 @@ function RenderCard({item, isLoading, errMess}) {
     }
     else if (errMess) {
         return(
-                <h4>{errMess}</h4>
+            <h4>{errMess}</h4>
         );
     }
     else
@@ -26,7 +26,6 @@ function RenderCard({item, isLoading, errMess}) {
                 </CardBody>
             </Card>
         );
-
 }
 
 function Home(props) {
@@ -34,7 +33,7 @@ function Home(props) {
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess} />
+                     <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}  />
                 </div>
                 <div className="col-12 col-md m-1">
                      <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
